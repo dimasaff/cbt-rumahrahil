ddd7d8dea6f5d628a03712f0a3c2b3c1a35c5786
sudrajadsasmita <sudrajad.sasmita@gmail.com>
2020-09-03 10:28:50 +0700

    slice login templates

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -5,6 +5,9 @@ class Login extends CI_Controller
 {
     public function index()
     {
+        $data['title'] = 'Halaman Login';
+        $this->load->view('templates/login_header', $data);
         $this->load->view('login/index');
+        $this->load->view('templates/login_footer');
     }
 }
---
+++ application/views/templates/login_footer.php
@@ -0,0 +1,24 @@
+</div>
+
+<!-- Footer -->
+<footer class="sticky-footer bg-primary fixed-bottom">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span class="text-light">Copyright &copy; Private Coding 2020</span>
+        </div>
+    </div>
+</footer>
+<!-- End of Footer -->
+<!-- Bootstrap core JavaScript-->
+<script src="<?= base_url('assets/'); ?>vendor/jquery/jquery.min.js"></script>
+<script src="<?= base_url('assets/'); ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="<?= base_url('assets/'); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="<?= base_url('assets/'); ?>js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
