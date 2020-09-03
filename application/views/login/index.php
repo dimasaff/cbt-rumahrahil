208a781e73817e7d5fc8d9e230fefd6580b9176c
sudrajadsasmita <sudrajad.sasmita@gmail.com>
2020-09-03 10:07:40 +0700

    first commit

---
+++ application/views/login/index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin 2 - Login</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="<?= base_url('assets/'); ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="<?= base_url('assets/'); ?>css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <!-- Image and text -->
+    <nav class="navbar navbar-dark bg-primary">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="<?= base_url('assets/'); ?>img/cms.png" width="30" height="30" class="d-inline-block align-top" alt="">
+                Rumah Rahil
+            </a>
+        </div>
+    </nav>
+    <div class="container">
+
+        <!-- Outer Row -->
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-lg-7">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Login Page</h1>
+                                    </div>
+
+                                    <form class="user" method="post" action="<?= base_url('login'); ?>">
+                                        <div class="form-group">
+                                            <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Enter Email Address..." value="<?= set_value('email'); ?>">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password">
+
+                                        </div>
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">
+                                            Login
+                                        </button>
+                                    </form>
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class="small" href="<?= base_url('auth/registration'); ?>">Create an Account!</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+    </div>
+
+    <!-- Footer -->
+    <footer class="sticky-footer bg-primary fixed-bottom">
+        <div class="container my-auto">
+            <div class="copyright text-center my-auto">
+                <span class="text-light">Copyright &copy; Private Coding 2020</span>
+            </div>
+        </div>
+    </footer>
+    <!-- End of Footer -->
+    <!-- Bootstrap core JavaScript-->
+    <script src="<?= base_url('assets/'); ?>vendor/jquery/jquery.min.js"></script>
+    <script src="<?= base_url('assets/'); ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="<?= base_url('assets/'); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="<?= base_url('assets/'); ?>js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
